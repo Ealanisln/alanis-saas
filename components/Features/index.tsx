456d54a0e8ce26a33207670ca283a55b2982f572
Emmanuel Alanis <ealanisln@me.com>
2023-04-16 11:38:24 -0700

    First commit

---
+++ components/Features/index.tsx
@@ -0,0 +1,30 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <>
+      <section
+        id="features"
+        className="bg-primary/[.03] py-16 md:py-20 lg:py-28"
+      >
+        <div className="container">
+          <SectionTitle
+            title="Main Features"
+            paragraph="Feel free to adjust the wording to fit your brand tone and voice, and highlight the specific features or benefits of your web design service that you want to emphasize."
+            center
+          />
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map((feature) => (
+              <SingleFeature key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Features;
