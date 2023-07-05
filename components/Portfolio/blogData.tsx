26316ed9601edcab8ecdded1640f6a54316332a7
Emmanuel Alanis <ealanisln@me.com>
2023-07-04 22:23:02 -0700

    added one card to portfolio

--- components/Portfolio/blogData.tsx
+++ components/Portfolio/blogData.tsx
@@ -6,7 +6,7 @@ const blogData: Blog[] = [
     title: "Conducir es fácil (Drive it's easy)",
     paragraph:
       "This website was a upgrade from HTML/CSS to Next JS, including login, database and permissions usage.",
-    image: "/images/blog/blog-01.jpg",
+    image: "/images/blog/blog-01.webp",
     location: {
       url: "https://conducir-es-facil.vercel.app/",
       image: "/images/blog/yo.jpg",
@@ -17,16 +17,16 @@ const blogData: Blog[] = [
   },
   {
     id: 2,
-    title: "Note Taker Web App - Using react hooks. ",
+    title: "Alanis Saas",
     paragraph:
-      "This web app was created from the original version JavaScript To-Do app, but this includes login and react hooks usage.",
-    image: "/images/blog/blog-02.jpg",
+      "This website was built using Next.js and Tailwind CSS. Nodemailer was used as the API for the contact form, making it fully responsive. Additionally, a webhook from Stripe will be included to facilitate subscription-based software acquisition.",
+    image: "/images/blog/blog-04.webp",
     location: {
-      url: "https://note-taker-wheat.vercel.app/",
+      url: "https://www.alanis.dev/",
       image: "/images/blog/yo.jpg",
-      designation: "NextJS, supabase, prisma.",
+      designation: "NextJS, Tailwind, Clerk",
     },
-    tags: ["Web app"],
+    tags: ["Landing Page"],
     publishDate: "2023",
   },
   {
@@ -34,7 +34,7 @@ const blogData: Blog[] = [
     title: "Sierra Western Store",
     paragraph:
       "This e-commerce was developed using Nextjs and postgreSQL, chakra UI and next auth for login.",
-    image: "/images/blog/blog-03.jpg",
+    image: "/images/blog/blog-03.webp",
     location: {
       url: "https://sierrra-western-store-git-navbar-ealanisln.vercel.app/",
       image: "/images/blog/yo.jpg",
@@ -43,5 +43,19 @@ const blogData: Blog[] = [
     tags: ["E-commerce"],
     publishDate: "2023",
   },
+  {
+    id: 4,
+    title: "Pokedex",
+    paragraph:
+      "Next.js-based Pokedex, a dynamic web app fetching data from a public API, fully responsive.",
+    image: "/images/blog/blog-02.webp",
+    location: {
+      url: "https://pokedex-alanis.vercel.app/",
+      image: "/images/blog/yo.jpg",
+      designation: "NextJS",
+    },
+    tags: ["Web app"],
+    publishDate: "2023",
+  }
 ];
 export default blogData;
