aee02ceb73c0ae7bbacb976b8944de34d28b299b
Emmanuel Alanis <ealanisln@me.com>
2023-07-30 16:09:02 -0700

    commit to rebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,13 @@ import Head from "next/head";
 import { ThemeProvider } from "next-themes";
 import RootLayout from "../layouts";
 import "../styles/index.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/nextjs";
+import { SubscriptionProvider } from "use-stripe-subscription";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   // Create a new supabase browser client on every first render.
@@ -14,17 +20,26 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <ClerkProvider {...pageProps}>
+      <SubscriptionProvider
+        stripePublishableKey={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
+      >
+        <ClerkProvider>
           <ThemeProvider
             attribute="class"
             enableSystem={true}
             defaultTheme="light"
           >
-            <RootLayout>
-              <Component {...pageProps} />
-            </RootLayout>
+            <SignedIn>
+              <RootLayout>
+                <Component {...pageProps} />
+              </RootLayout>
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
           </ThemeProvider>
-      </ClerkProvider>
+        </ClerkProvider>
+      </SubscriptionProvider>
     </>
   );
 }
