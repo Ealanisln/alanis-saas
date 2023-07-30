9af1fbdef66dad25d8596619d583dd533b1e1e02
Emmanuel Alanis <ealanisln@me.com>
2023-07-30 16:11:09 -0700

    commit to rebase

--- pages/api/prepare.tsx
+++ pages/api/prepare.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
@@ -11,10 +12,15 @@ function getBaseUrl(req: NextApiRequest) {
   const host = req.headers["x-forwarded-host"] || req.headers.host || "localhost:3000";
   return `${protocol}://${host}`;
 }
+=======
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Stripe from 'stripe';
+>>>>>>> 3e6673a (pricing module working with stripe)
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {} as any);
 
 export default async function handler(
+<<<<<<< HEAD
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -55,4 +61,38 @@ export default async function handler(
     console.error('Error creating checkout session:', error.message);
     return res.status(500).json({ error: 'Internal server error' });
   }
+=======
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== 'GET') {
+        return res.status(405).json({ error: "Method not allowed" });
+    }
+
+    if (!req.query.price || +req.query.price <= 0) {
+        return res.status(400).json({ error: "Please enter a valid price" });
+    }
+
+    const { url } = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+            {
+                price_data: {
+                    currency: "USD",
+                    product_data: {
+                        name: "GrowChief",
+                        description: `Charging you`,
+                    },
+                    unit_amount: 100 * +req.query.price,
+                },
+                quantity: 1,
+            },
+        ],
+        mode: "payment",
+        success_url: "http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: "http://localhost:3000",
+    });
+
+    return res.json({ url });
+>>>>>>> 3e6673a (pricing module working with stripe)
 }
