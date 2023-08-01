d3583f9707e32d2418b48f3628995dd2437de158
Emmanuel Alanis <ealanisln@me.com>
2023-08-01 14:42:54 -0700

    checkpoint commit;

--- utils/get-stripejs.ts
+++ utils/get-stripejs.ts
@@ -1,7 +1,8 @@
 // ./utils/get-stripejs.ts
-import { Stripe, loadStripe } from '@stripe/stripe-js';
+import { loadStripe, Stripe } from '@stripe/stripe-js'; // Import the 'Stripe' type
 
 let stripePromise: Promise<Stripe | null>;
+
 const getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
