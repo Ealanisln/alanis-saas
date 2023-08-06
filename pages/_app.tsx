f9ff66d1a439e7c690eef61b712df9a8771ba90d
Emmanuel Alanis <ealanisln@me.com>
2023-08-05 17:48:05 -0700

    removed stripe subcriptions from _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "next-themes";
 import RootLayout from "../layouts";
 import "../styles/index.css";
-
-import { SubscriptionProvider } from "use-stripe-subscription";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   // Create a new supabase browser client on every first render.
@@ -15,19 +12,12 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <SubscriptionProvider
-        stripePublishableKey={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
-      >
-        <ThemeProvider
-          attribute="class"
-          enableSystem={true}
-          defaultTheme="light"
-        >
-          <RootLayout>
-            <Component {...pageProps} />
-          </RootLayout>
-        </ThemeProvider>
-      </SubscriptionProvider>
+
+      <ThemeProvider attribute="class" enableSystem={true} defaultTheme="light">
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+      </ThemeProvider>
     </>
   );
 }
