cbcfb8fef44b8ba3d8603deae18849e76084a972
Emmanuel Alanis <ealanisln@me.com>
2023-10-16 10:09:11 -0700

    updated stripe variables to live mode

--- pages/cancel/index.tsx
+++ pages/cancel/index.tsx
@@ -1,4 +1,5 @@
 import SectionTitle from "@/components/Common/SectionTitle";
+import { GetStaticPropsContext } from "next";
 import Link from "next/link";
 
 function Cancel() {
@@ -24,3 +25,14 @@ function Cancel() {
 }
 
 export default Cancel;
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  return {
+    props: {
+      // You can get the messages from anywhere you like. The recommended
+      // pattern is to put them in JSON files separated by locale and read
+      // the desired one based on the `locale` received from Next.js.
+      messages: (await import(`../../locales/${locale}.json`)).default,
+    },
+  };
+}
--- pages/success/index.tsx
+++ pages/success/index.tsx
@@ -5,6 +5,7 @@ import SectionTitle from "@/components/Common/SectionTitle";
 import Image from "next/image";
 import { useRouter } from "next/router"; // Import the useRouter hook
 import Axios from "axios";
+import { GetStaticPropsContext } from "next";
 
 interface SessionData {
   metadata: {
@@ -124,3 +125,14 @@ const Success = () => {
 };
 
 export default Success;
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  return {
+    props: {
+      // You can get the messages from anywhere you like. The recommended
+      // pattern is to put them in JSON files separated by locale and read
+      // the desired one based on the `locale` received from Next.js.
+      messages: (await import(`../../locales/${locale}.json`)).default,
+    },
+  };
+}
